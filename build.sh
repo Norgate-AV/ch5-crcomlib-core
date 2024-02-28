1840df92b27c50d2bd93f9e4e8e064673af22a63
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-02-28 00:26:19 +0000

    refactor: only run esm build for now

--- build.sh
+++ build.sh
@@ -15,7 +15,9 @@ if [[ "${SHOULD_BUILD}" == "yes" ]]; then
         exit 1
     }
 
-    npm run build:prod_all
+    # npm run build:prod_all
+    npm run build:prod:esm
+    npm run build:prod:esm-no-ce
 
     pwd
     ls -la ./build_bundles
