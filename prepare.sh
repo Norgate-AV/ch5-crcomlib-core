7e6c198e9d820fb233ed37c578d9f551b997858f
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-02-28 14:24:46 +0000

    fix: use setpath_json to reset repository.url

--- prepare.sh
+++ prepare.sh
@@ -53,10 +53,7 @@ setpath "package" "name" "@norgate-av/ch5-crcomlib"
 setpath "package" "version" "$(echo "${RELEASE_VERSION}" | sed -n -E "s/^(.*)\.([0-9]+)$/\1/p")"
 setpath "package" "author" "Norgate AV"
 setpath "package" "license" "MIT"
-setpath "package" "repository.url" "https://github.com/Norgate-AV/ch5-crcomlib-core.git"
-# setpath "package" "release" "$(echo "${RELEASE_VERSION}" | sed -n -E "s/^(.*)\.([0-9]+)$/\2/p")"
-
-# replace 's|Crestron|Norgate AV|' package.json
+setpath_json "package" "repository.url" "https://github.com/Norgate-AV/ch5-crcomlib-core.git"
 
 cat package.json
 
